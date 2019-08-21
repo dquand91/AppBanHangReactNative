722d8f7da34786b1e4855b72dcf03dee84145300
Luong Quan <luong.quan@mulodo.com>
2019-08-21 17:48:44 +0700

    - Add Image resource - Apply BottomTabNavigator - Create Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+} from 'react-navigation';
 
 // Nếu ở đây sửa tên file "Authencation.js" thành "index.js"
 // Thì mình chỉ cần import import Authencation from './src/components/Authencation';
@@ -9,39 +13,120 @@ import ChangeInfo from './Info/ChangeInfo';
 import Main from './Main/Main';
 import Order from './Order/Order';
 
-const MyRouter = createStackNavigator(
-  {
-    Screen_Main: {
-      screen: Main,
-      navigationOptions: {
-        headerTitle: 'Main',
-      },
-    },
-    Screen_Authentication: {
-      screen: Authencation,
-      navigationOptions: {
-        headerTitle: 'Authencation',
-      },
-    },
-    Screen_ChangeInfo: {
-      screen: ChangeInfo,
-      navigationOptions: {
-        headerTitle: 'ChangeInfo',
-      },
-    },
-    Screen_Order: {
-      screen: Order,
-      navigationOptions: {
-        headerTitle: 'OrderHistory',
+import {Image} from 'react-native';
+
+import Cart from './Main/Shop/Cart/Cart';
+import Search from './Main/Shop/Search/Search';
+import Contact from './Main/Shop/Contact/Contact';
+
+const myRouterConfigs = {
+  Tab_Home: {
+    screen: Main,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      // Callback để xử lý hiển thị khi tab được chọn
+      // Nếu đang được chọn thì show hình khác, bình thường show hình khác
+      tabBarIcon: ({focused}) => {
+        const icon1 = require('../assets/appIcon/home.png');
+        const icon2 = require('../assets/appIcon/home0.png');
+
+        const icon = focused ? icon1 : icon2;
+        return <Image source={icon} style={{width: 25, height: 25}} />;
       },
     },
   },
-  {
-    initialRouteName: 'Screen_Main',
+  Tab_Cart: {
+    screen: Cart,
+    navigationOptions: {
+      tabBarLabel: 'Cart',
+      tabBarIcon: ({focused}) => {
+        const icon1 = require('../assets/appIcon/cart.png');
+        const icon2 = require('../assets/appIcon/cart0.png');
+
+        const icon = focused ? icon1 : icon2;
+        return <Image source={icon} style={{width: 25, height: 25}} />;
+      },
+    },
   },
+  Tab_Search: {
+    screen: Search,
+    navigationOptions: {
+      tabBarLabel: 'Search',
+      tabBarIcon: ({focused}) => {
+        const icon1 = require('../assets/appIcon/search.png');
+        const icon2 = require('../assets/appIcon/search0.png');
+
+        const icon = focused ? icon1 : icon2;
+        return <Image source={icon} style={{width: 25, height: 25}} />;
+      },
+    },
+  },
+  Tab_Contact: {
+    screen: Contact,
+    navigationOptions: {
+      tabBarLabel: 'Contact',
+      tabBarIcon: ({focused}) => {
+        const icon1 = require('../assets/appIcon/contact.png');
+        const icon2 = require('../assets/appIcon/contact0.png');
+
+        const icon = focused ? icon1 : icon2;
+        return <Image source={icon} style={{width: 25, height: 25}} />;
+      },
+    },
+  },
+};
+
+const myBottomTabNavigatorConfig = {
+  tabBarOptions: {
+    initialRouteName: 'Tab_Home',
+    // Màu  khi tab được chọn
+    activeTintColor: 'green',
+    inactiveBackgroundColor: '',
+    // Màu  khi tab chưa được chọn
+    inactiveTintColor: 'gray',
+  },
+};
+
+const myBottomTabNavigator = createBottomTabNavigator(
+  myRouterConfigs,
+  myBottomTabNavigatorConfig,
 );
 
-const AppContainer = createAppContainer(MyRouter);
+const AppContainer = createAppContainer(myBottomTabNavigator);
+
+// const MyRouter = createStackNavigator(
+//   {
+//     Screen_Main: {
+//       screen: Main,
+//       navigationOptions: {
+//         headerTitle: 'Main',
+//       },
+//     },
+//     Screen_Authentication: {
+//       screen: Authencation,
+//       navigationOptions: {
+//         headerTitle: 'Authencation',
+//       },
+//     },
+//     Screen_ChangeInfo: {
+//       screen: ChangeInfo,
+//       navigationOptions: {
+//         headerTitle: 'ChangeInfo',
+//       },
+//     },
+//     Screen_Order: {
+//       screen: Order,
+//       navigationOptions: {
+//         headerTitle: 'OrderHistory',
+//       },
+//     },
+//   },
+//   {
+//     initialRouteName: 'Screen_Main',
+//   },
+// );
+
+// const AppContainer = createAppContainer(MyRouter);
 
 export default class App extends Component {
   render() {
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import MyTouchable from '../CustomView/MyTouchable';
-
+import Header from './Shop/Header';
 export default class Main extends Component {
   render() {
     const {navigation} = this.props;
@@ -11,24 +11,28 @@ export default class Main extends Component {
         style={{
           flex: 1,
           backgroundColor: 'lightskyblue',
-          alignItems: 'center',
         }}>
-        <Text>Main</Text>
-        {/* MyTouchable là do mình custom, cần 2 props (tham số)
-        inputNavigation: để chứa cái navigation
-        inputScreenName: để chứa cái tên màn hình muốn tới  */}
-        <MyTouchable
-          inputNavigation={navigation}
-          inputGoToScreenName="Screen_Authentication"
-        />
-        <MyTouchable
-          inputNavigation={navigation}
-          inputGoToScreenName="Screen_Order"
-        />
-        <MyTouchable
-          inputNavigation={navigation}
-          inputGoToScreenName="Screen_ChangeInfo"
-        />
+        <View>
+          <Header />
+        </View>
+        <View>
+          <Text style={{color: '#ff0080'}}> Main component </Text>
+          {/* MyTouchable là do mình custom, cần 2 props (tham số)
+          inputNavigation: để chứa cái navigation
+          inputScreenName: để chứa cái tên màn hình muốn tới  */}
+          <MyTouchable
+            inputNavigation={navigation}
+            inputGoToScreenName="Screen_Authentication"
+          />
+          <MyTouchable
+            inputNavigation={navigation}
+            inputGoToScreenName="Screen_Order"
+          />
+          <MyTouchable
+            inputNavigation={navigation}
+            inputGoToScreenName="Screen_ChangeInfo"
+          />
+        </View>
       </View>
     );
   }
